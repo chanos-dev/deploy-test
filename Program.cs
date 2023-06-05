a5d8e7f8a9cc58bb4c2cac8cf74abfb57e7bd72c
chbaek <chbaek@akuo.ai>
2023-06-05 13:58:38 +0900

    deploy test

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
+
 var app = builder.Build();
  
 app.UseSwagger();
@@ -15,5 +17,6 @@ app.UseSwaggerUI();
 app.UseAuthorization();
 
 app.MapControllers();
+
 
 app.Run();
